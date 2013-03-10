437c00fab354d74c5dbf2bdfc15cc2f06567fc79
dengchengqi <Woshi13811317327>
2013-03-10 21:21:21 +0800

    Initial Commit

---
+++ TelecomProperty/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TelecomProperty
+//
+//  Created by 邓成其 on 13-3-10.
+//  Copyright (c) 2013年 chutangkeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TelecomProperty/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TelecomProperty
+//
+//  Created by 邓成其 on 13-3-10.
+//  Copyright (c) 2013年 chutangkeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
